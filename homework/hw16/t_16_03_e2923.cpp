5b1024432834cbdaa645efe1736db5ea9b949a2a
ShakunAnastasia <shakunanastasia19292@gmail.com>
2026-04-12 21:43:34 +0300

    task 16.3 code (correct version)
    (не дочитала умову :( )

--- homework/hw16/t_16_03_e2923.cpp
+++ homework/hw16/t_16_03_e2923.cpp
@@ -1,85 +1,96 @@
-#include <cstdio>
+#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <stack>
 
 using namespace std;
 
-const int MAXN = 1000005;
-vector<int> adj[MAXN];
-int col[MAXN], tin[MAXN], tout[MAXN], timer, order[MAXN];
-int bit[MAXN], last_pos[MAXN], res[MAXN];
+#pragma GCC optimize("Ofast")
 
-struct Query {
-    int l, r, id;
+struct Node {
+    int id;
+    int color;
+    int tin, tout;
+    vector<Node*> children;
 };
+
+Node nodes[1000005];
+int timer = 0;
+int dfs_order[1000005];
+int bit[1000005];
+int last_pos[1000005];
+int result[1000005];
 
 void update(int i, int val, int n) {
     for (; i <= n; i += i & -i) bit[i] += val;
 }
 
-int get_sum(int i) {
+int query(int i) {
     int s = 0;
     for (; i > 0; i -= i & -i) s += bit[i];
     return s;
 }
 
+void buildDFS(Node* v) {
+    v->tin = ++timer;
+    dfs_order[timer] = v->id;
+    for (Node* child : v->children) {
+        buildDFS(child);
+    }
+    v->tout = timer;
+}
+
+struct Query {
+    int l, r, id;
+};
+
+bool compareQueries(const Query& a, const Query& b) {
+    if (a.r != b.r) return a.r < b.r;
+    return a.l < b.l;
+}
+
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n;
-    if (scanf("%d", &n) != 1) return 0;
-    int root = -1;
+    if (!(cin >> n)) return 0;
+
+    int root_id = 0;
     for (int i = 1; i <= n; ++i) {
         int p, c;
-        scanf("%d %d", &p, &c);
-        col[i] = c;
-        if (p == 0) root = i;
-        else adj[p].push_back(i);
+        cin >> p >> c;
+        nodes[i].id = i;
+        nodes[i].color = c;
+        if (p == 0) root_id = i;
+        else nodes[p].children.push_back(&nodes[i]);
     }
 
-    stack<pair<int, int>> st;
-    st.push({root, 0});
-    while (!st.empty()) {
-        int u = st.top().first;
-        int &idx = st.top().second;
-        if (idx == 0) {
-            tin[u] = ++timer;
-            order[timer] = u;
-        }
-        if (idx < adj[u].size()) {
-            st.push({adj[u][idx++], 0});
-        } else {
-            tout[u] = timer;
-            st.pop();
-        }
-    }
+    buildDFS(&nodes[root_id]);
 
     vector<Query> q(n);
     for (int i = 1; i <= n; ++i) {
-        q[i - 1] = {tin[i], tout[i], i};
+        q[i - 1] = {nodes[i].tin, nodes[i].tout, i};
     }
 
-    sort(q.begin(), q.end(), [](const Query& a, const Query& b) {
-        return a.r < b.r;
-    });
+    sort(q.begin(), q.end(), compareQueries);
 
-    int curr_r = 0;
+    int current_r = 0;
     for (int i = 0; i < n; ++i) {
-        while (curr_r < q[i].r) {
-            curr_r++;
-            int u = order[curr_r];
-            int c = col[u];
-            if (last_pos[c]) {
-                update(last_pos[c], -1, n);
-            }
-            update(curr_r, 1, n);
-            last_pos[c] = curr_r;
+        while (current_r < q[i].r) {
+            current_r++;
+            int node_id = dfs_order[current_r];
+            int c = nodes[node_id].color;
+            if (last_pos[c]) update(last_pos[c], -1, n);
+            update(current_r, 1, n);
+            last_pos[c] = current_r;
         }
-        res[q[i].id] = get_sum(q[i].r) - get_sum(q[i].l - 1);
+        result[q[i].id] = query(q[i].r) - query(q[i].l - 1);
     }
 
     for (int i = 1; i <= n; ++i) {
-        printf("%d%c", res[i], (i == n ? '\n' : ' '));
+        cout << result[i] << (i == n ? "" : " ");
     }
+    cout << endl;
 
     return 0;
 }
